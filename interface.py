edf0222e09deeab47dcd36ef1845a704cde46cca
busitaro <busitaro10@gmail.com>
2021-10-03 12:19:35 +0900

    :sparkles: add param to SampleLogics' __init__

--- interface.py
+++ interface.py
@@ -6,12 +6,24 @@ class ISample(metaclass=ABCMeta):
     def name(self):
         raise NotImplementedError()
 
+    @abstractmethod
+    def show_param(self):
+        raise NotImplementedError()
+
 
 class SampleLogic1(ISample):
+    def __init__(self, param: str):
+        self.__param = param
+
     def name(self):
         print('This is SampleLogic1')
 
+    def show_param(self):
+        print('SampleLogic1 has a param with a value of {}'.format(self.__param))
 
 class SampleLogic2(ISample):
     def name(self):
         print('This is SampleLogic2')
+
+    def show_param(self):
+        print('SampleLogic2 has a param with a value of {}'.format(self.__param))
